76faa605171384f172fcda868ceb941b885f9100
Kirill Ryabov <sailor.kirill@gmail.com>
2023-08-27 18:23:09 +0700

    Create sql file to initialize tables

---
+++ sql/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS segment (
+    id SERIAL PRIMARY KEY,
+    slug TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS users_segments (
+    user_id INTEGER NOT NULL,
+    segment_id SERIAL REFERENCES segment (id) ON DELETE CASCADE,
+    delete_time TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS logs (
+    user_id INTEGER NOT NULL,
+    slug TEXT NOT NULL,
+    operation TEXT NOT NULL,
+    request_time TIMESTAMP NOT NULL
+);
