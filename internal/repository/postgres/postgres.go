b31cd75b4a9b39dcc25a48b5a73d2e4cdee0488b
Kirill Ryabov <sailor.kirill@gmail.com>
2023-08-27 18:23:48 +0700

    Create initDB function

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -3,18 +3,31 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/kiryu-dev/segments-api/internal/config"
+	_ "github.com/lib/pq"
 )
 
 func New(cfg *config.DB) (*sql.DB, error) {
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	db, err := sql.Open("postgres", cfg.String())
 	if err != nil {
 		return nil, fmt.Errorf("invalid connection to postgres: %w", err)
 	}
 	if err := db.Ping(); err != nil {
 		return nil, fmt.Errorf("cannot get access to postgtes: %w", err)
 	}
+	if err := initDB(db, cfg.InitFilepath); err != nil {
+		return nil, fmt.Errorf("cannot initialize tables: %w", err)
+	}
 	return db, nil
+}
+
+func initDB(db *sql.DB, initFilepath string) error {
+	buf, err := os.ReadFile(initFilepath)
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec(string(buf))
+	return err
 }
