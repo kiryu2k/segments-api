499fec547a996dd73cdb68f9014b0fc23c45e701
Kirill Ryabov <sailor.kirill@gmail.com>
2023-08-28 12:36:37 +0700

    Implement first version of adding segment to user function

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -1,9 +1,11 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/kiryu-dev/segments-api/internal/config"
 	_ "github.com/lib/pq"
@@ -14,20 +16,22 @@ func New(cfg *config.DB) (*sql.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("invalid connection to postgres: %w", err)
 	}
-	if err := db.Ping(); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
 		return nil, fmt.Errorf("cannot get access to postgtes: %w", err)
 	}
-	if err := initDB(db, cfg.InitFilepath); err != nil {
+	if err := initDB(ctx, db, cfg.InitFilepath); err != nil {
 		return nil, fmt.Errorf("cannot initialize tables: %w", err)
 	}
 	return db, nil
 }
 
-func initDB(db *sql.DB, initFilepath string) error {
+func initDB(ctx context.Context, db *sql.DB, initFilepath string) error {
 	buf, err := os.ReadFile(initFilepath)
 	if err != nil {
 		return err
 	}
-	_, err = db.Exec(string(buf))
+	_, err = db.ExecContext(ctx, string(buf))
 	return err
 }
--- internal/repository/segment.go
+++ internal/repository/segment.go
@@ -42,7 +42,35 @@ func (s *segmentRepository) Delete(ctx context.Context, slug string) error {
 }
 
 func (s *segmentRepository) AddToUser(ctx context.Context, seg *model.UserSegments) error {
-	return nil
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	var (
+		searchQuery = `SELECT id FROM segment WHERE slug = $1`
+		insertQuery = `
+INSERT INTO users_segments (user_id, segment_id, delete_time)
+VALUES ($1, $2, $3);
+		`
+	)
+	for _, segment := range seg.Segments {
+		var id uint64
+		err := tx.QueryRowContext(ctx, searchQuery, segment.Slug).Scan(&id)
+		if err != nil {
+			if rErr := tx.Rollback(); rErr != nil {
+				return fmt.Errorf("transaction error: %w, rollback error: %w", err, rErr)
+			}
+			return ErrSegmentNotExists
+		}
+		_, err = tx.ExecContext(ctx, insertQuery, seg.UserID, id, segment.DeleteTime)
+		if err != nil {
+			if rErr := tx.Rollback(); rErr != nil {
+				return fmt.Errorf("transaction error: %w, rollback error: %w", err, rErr)
+			}
+			return err
+		}
+	}
+	return tx.Commit()
 }
 
 func (s *segmentRepository) DeleteFromUser(ctx context.Context, seg *model.UserSegments) error {
