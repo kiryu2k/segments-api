105ae55e4d30833bc0c77224371171a6743ae984
Kirill Ryabov <sailor.kirill@gmail.com>
2023-08-28 12:37:05 +0700

    Edit user-segment model

--- internal/model/models.go
+++ internal/model/models.go
@@ -1,6 +1,13 @@
 package model
 
+import "time"
+
+type Segment struct {
+	Slug       string     `json:"slug"`
+	DeleteTime *time.Time `json:"delete_time"`
+}
+
 type UserSegments struct {
-	UserID uint64   `json:"user_id"`
-	Slugs  []string `json:"slugs"`
+	UserID   uint64    `json:"user_id"`
+	Segments []Segment `json:"segments"`
 }
